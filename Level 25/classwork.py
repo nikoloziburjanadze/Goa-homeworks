6fdd6d8df7e1a0d2255957d58255de7827d97a02
nikoloziburjanadze <nikoloziburjanadze0909@gmail.com>
2024-10-27 21:58:46 +0400

    commit

---
+++ Level 25/classwork.py
@@ -0,0 +1,42 @@
+# i think its 4
+
+list = [8,4,2,6]
+list.remove(2)
+print(len(list)+list.count(6))
+
+
+
+
+
+
+#i think its 4
+
+nums=[2,4,8,9,5]
+
+nums.insert(1, 3)
+
+nums.remove(9)
+
+nums.insert(0, nums.count(8))
+
+print(nums[3])
+
+#4
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+print(numbers.append(100))
+print(numbers.insert(0, 5))
+print(numbers.remove(30))
+print(numbers.sort())
+print(numbers.reverse())
+print(numbers.index(50))
+print(numbers.count(20))
+
+#5
+numbers = [10, 20, 30, 40, 50]
+print(numbers)
+print(numbers.append(60))
+print(numbers.remove(30))
+print(([0]+[1]+[2]+[3]+[4])*2)
+
+
---
+++ Level 25/homework.py
@@ -0,0 +1,3 @@
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+print(fruits)
+print(fruits([0]))
